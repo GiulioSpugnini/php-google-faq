8f4bbe049ed4ac67a04206ba584d04023d286e35
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-02-28 15:13:12 +0100

    printed all faqs' elements

--- faq.php
+++ faq.php
@@ -56,9 +56,18 @@ $faqs = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Faq</title>
 </head>
 <body>
-    
+    <?php foreach($faqs as $faq): ?>
+
+        <h2>
+            <?=  print_r($faq['question'])  ?>
+        </h2>
+        <p>
+            <?=  print_r($faq['answer'])  ?>
+        </p>
+
+        <?php endforeach; ?>
 </body>
 </html>
