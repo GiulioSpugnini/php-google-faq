2b8c07037f83d5bfa9aafb8cbeb882e5d461c3aa
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-02-28 15:09:21 +0100

    faqs variable implemented

--- faq.php
+++ faq.php
@@ -31,7 +31,13 @@ $faqs = [
         Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
         Google LLC, con sede negli Stati Uniti, per il resto del mondo.
         La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
-        Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account."],
+        Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.
+        Stabilire il paese associato al tuo account
+        Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.
+
+        I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.
+
+        Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato."],
     ],
     [
         'question' => ["Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?"],
@@ -43,3 +49,16 @@ $faqs = [
     ],
 
 ];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
