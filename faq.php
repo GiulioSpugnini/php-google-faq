ad954801cb3f066866a05f4041dd9be6f6da2fb4
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-02-28 15:39:54 +0100

    used echo to print the elements

--- faq.php
+++ faq.php
@@ -52,22 +52,25 @@ $faqs = [
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Faq</title>
 </head>
+
 <body>
-    <?php foreach($faqs as $faq): ?>
+    <?php foreach ($faqs as $faq) : ?>
 
         <h2>
-            <?=  print_r($faq['question'])  ?>
+            <?= $faq['question'] ?>
         </h2>
         <p>
-            <?=  print_r($faq['answer'])  ?>
+            <?= $faq['answer'] ?>
         </p>
 
-        <?php endforeach; ?>
+    <?php endforeach; ?>
 </body>
+
 </html>
